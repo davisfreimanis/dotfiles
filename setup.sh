c3c9ce3edd283abc4a368bb6a8a3bdc566d07cdf
Davis Freimanis <davisfreimanis@gmail.com>
2018-03-21 17:19:24 +0100

    Added zsh configuration

--- setup.sh
+++ setup.sh
@@ -4,8 +4,10 @@ dotfiles_dir=~/dotfiles
 
 # Deleting old dotfiles
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
 
 # Linking new dotfiles
-ln -sf $dotfiles_dir/vimrc ~/.vimrc
+ln -sf $dotfiles_dir/vim/vimrc ~/.vimrc
+ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
 
 echo "Installation complete"
