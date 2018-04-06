bc430c268a0c47cbcd71aecad9d516c961cd624c
Davis Freimanis <davisfreimanis@gmail.com>
2018-04-06 13:02:28 +0200

    added vundle

--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@
 dotfiles_dir=~/dotfiles
 
 # Deleting old dotfiles
+sudo rm -rm ~/.vim > /dev/null 2>&1
 sudo rm -rf ~/.vimrc > /dev/null 2>&1
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 
 # Linking new dotfiles
+ln -sf $dotfiles_dir/vim/bundle ~/.vim/bundle
 ln -sf $dotfiles_dir/vim/vimrc ~/.vimrc
 ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
 ln -sf $dotfiles_dir/tmux/tmux.conf ~/.tmux.conf
