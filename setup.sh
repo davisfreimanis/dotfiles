76ea6ada2568a775ce6010b59abd93982cae73ee
Davis Freimanis <davisfreimanis@gmail.com>
2018-04-16 22:56:14 +0200

    pep8

--- setup.sh
+++ setup.sh
@@ -9,8 +9,10 @@ sudo rm -rf ~/.zshrc > /dev/null 2>&1
 sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
 
 # Linking new dotfiles
-ln -sf $dotfiles_dir/vim/bundle ~/.vim/bundle
 ln -sf $dotfiles_dir/vim/vimrc ~/.vimrc
+ln -sf $dotfiles_dir/vim/bundle ~/.vim/bundle
+echo "Vundle installed. Run :PluginInstall to install plugins"
+
 ln -sf $dotfiles_dir/zsh/zshrc ~/.zshrc
 ln -sf $dotfiles_dir/tmux/tmux.conf ~/.tmux.conf
 
