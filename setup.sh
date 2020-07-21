2cc57bb4b591fbdff91adfee15a19e774b1bffd8
Davis Freimanis <davis@freiman.is>
2020-07-21 11:45:44 +0200

    relative numbers vim and backup for setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 dotfiles_dir=~/dotfiles
+backup_dir=~/dotfile_backup
 
 # Cloning submodules
 git submodule init
@@ -10,12 +11,21 @@ git submodule update
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Deleting old dotfiles
-sudo rm -rf ~/.vim > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-sudo rm -rf ~/.zshrc > /dev/null 2>&1
-sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
-sudo rm -rf ~/.gitconfig > /dev/null 2>&1
-sudo rm -rf ~/.gitconfig.local > /dev/null 2>&1
+# sudo rm -rf ~/.vim > /dev/null 2>&1
+# sudo rm -rf ~/.vimrc > /dev/null 2>&1
+# sudo rm -rf ~/.zshrc > /dev/null 2>&1
+# sudo rm -rf ~/.tmux.conf > /dev/null 2>&1
+# sudo rm -rf ~/.gitconfig > /dev/null 2>&1
+# sudo rm -rf ~/.gitconfig.local > /dev/null 2>&1
+
+# Creating a backup of old dotfiles
+mkdir -p $backup_dir
+mv ~/.vim $backup_dir/.vim
+mv ~/.vimrc $backup_dir/.vimrc 
+mv ~/.zshrc $backup_dir/.zshrc
+mv ~/.tmux.conf $backup_dir/.tmux.conf
+mv ~/.gitconfig $backup_dir/.gitconfig
+mv ~/.gitconfig.local $backup_dir/.gitconfig.local
 
 # Linking new dotfiles
 ln -sf $dotfiles_dir/vim/vimrc ~/.vimrc
